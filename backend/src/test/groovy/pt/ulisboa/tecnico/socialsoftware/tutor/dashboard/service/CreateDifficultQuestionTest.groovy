1ee80ca86c585c201fdd2036da84236d76a8ada1
Joao <joao.cruz.galego@tecnico.ulisboa.pt>
2022-03-16 18:37:42 +0000

    closes #16; F1.2 Create Implemented Test Cases - Difficult Question

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/service/CreateDifficultQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/service/CreateDifficultQuestionTest.groovy
@@ -21,25 +21,140 @@ import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.QU
 
 @DataJpaTest
 class CreateDifficultQuestionTest extends SpockTest {
+    def student
+    def dashboard
+    def question
 
     def setup() {
+        createExternalCourseAndExecution()
+
+        student = new Student(USER_1_NAME, false)
+        student.addCourse(externalCourseExecution)
+        userRepository.save(student)
+
+        question = new Question()
+        question.setTitle(QUESTION_1_TITLE)
+        question.setContent(QUESTION_1_CONTENT)
+        question.setStatus(Question.Status.AVAILABLE)
+        question.setNumberOfAnswers(2)
+        question.setNumberOfCorrect(1)
+        question.setCourse(externalCourse)
+        def questionDetails = new MultipleChoiceQuestion()
+        question.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+        questionRepository.save(question)
+
+        dashboard = new Dashboard(externalCourseExecution, student)
+        dashboardRepository.save(dashboard)
     }
 
-    def "create difficult question with percentage #percentage"() {
+    @Unroll
+    def "create difficult question with percentage"() {
+        when:
+        difficultQuestionService.createDifficultQuestions(dashboard.getId(), question.getId(), percentage)
+
+        then:
+        difficultQuestionRepository.count() == 1L
+        def result = difficultQuestionRepository.findAll().get(0)
+        result.getId() != null
+        result.getDashboard().getId() == dashboard.getId()
+        result.getQuestion().getId() == question.getId()
+        result.isRemoved() == false
+        result.getRemovedDate() == null
+        result.getPercentage() == percentage
+        and:
+        def dashboard = dashboardRepository.getById(dashboard.getId())
+        dashboard.getDifficultQuestions().contains(result)
+        dashboard.getLastCheckDifficultQuestions().isAfter(DateHandler.now().minusMinutes(1))
+
+        where:
+        percentage << [0, 12, 24]
     }
 
     def "cannot create two difficult questions for the same question"() {
+        given:
+        difficultQuestionService.createDifficultQuestions(dashboard.getId(), question.getId(), 13)
+
+        when:
+        difficultQuestionService.createDifficultQuestions(dashboard.getId(), question.getId(), 24)
+
+        then:
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.DIFFICULT_QUESTION_ALREADY_CREATED
+        and:
+        difficultQuestionRepository.count() == 1L
     }
 
     def "cannot create a difficult question that does not belong to the student course"() {
+        given:
+        def alienCourse = new Course(COURSE_1_NAME, Course.Type.TECNICO)
+        courseRepository.save(alienCourse)
+        and:
+        def alienQuestion = new Question()
+        alienQuestion.setTitle(QUESTION_1_TITLE)
+        alienQuestion.setContent(QUESTION_1_CONTENT)
+        alienQuestion.setStatus(Question.Status.AVAILABLE)
+        alienQuestion.setNumberOfAnswers(2)
+        alienQuestion.setNumberOfCorrect(1)
+        alienQuestion.setCourse(alienCourse)
+        def questionDetails = new MultipleChoiceQuestion()
+        alienQuestion.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+        questionRepository.save(alienQuestion)
+
+        when:
+        difficultQuestionService.createDifficultQuestions(dashboard.getId(), alienQuestion.getId(), 22)
+
+        then:
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.CANNOT_CREATE_DIFFICULT_QUESTION
+        and:
+        difficultQuestionRepository.count() == 0L
     }
 
+    @Unroll
     def "cannot create difficult question with invalid percentage"() {
+        when:
+        difficultQuestionService.createDifficultQuestions(dashboard.getId(), question.getId(), percentage)
+
+        then:
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == CANNOT_CREATE_DIFFICULT_QUESTION
+        difficultQuestionRepository.count() == 0L
+
+        where:
+        percentage << [-100, -1, 25, 50, 150]
+
     }
 
+    @Unroll
     def "cannot create difficult question with invalid dashboard id"() {
+        when:
+        difficultQuestionService.createDifficultQuestions(dashboardId, question.getId(), 20)
+
+        then:
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == DASHBOARD_NOT_FOUND
+        difficultQuestionRepository.count() == 0L
+
+        where:
+        dashboardId << [0, 100]
     }
 
+    @Unroll
     def "cannot create difficult question with invalid question id"() {
+        when:
+        difficultQuestionService.createDifficultQuestions(dashboard.getId(), questionId, 20)
+
+        then:
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == QUESTION_NOT_FOUND
+        difficultQuestionRepository.count() == 0L
+
+        where:
+        questionId << [0, 100]
     }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
 }
