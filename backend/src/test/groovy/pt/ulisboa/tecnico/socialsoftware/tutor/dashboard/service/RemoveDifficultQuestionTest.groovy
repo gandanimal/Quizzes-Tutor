84fea5f4b5cb4ed38cc10451ee4db3cb3ad2aad2
LiFe1007 <lucas.figueiredo@tecnico.ulisboa.pt>
2022-03-16 18:20:43 +0000

    closes #15; F1.5 Remove Implemented Test Cases - Difficult Question

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/service/RemoveDifficultQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/service/RemoveDifficultQuestionTest.groovy
@@ -27,19 +27,108 @@ class RemoveDifficultQuestionTest extends SpockTest {
     def optionKO
 
     def setup() {
-        
+        createExternalCourseAndExecution()
+
+        student = new Student(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.EXTERNAL)
+        student.addCourse(externalCourseExecution)
+        userRepository.save(student)
+
+        question = new Question()
+        question.setKey(1)
+        question.setTitle(QUESTION_1_TITLE)
+        question.setContent(QUESTION_1_CONTENT)
+        question.setStatus(Question.Status.AVAILABLE)
+        question.setNumberOfAnswers(2)
+        question.setNumberOfCorrect(1)
+        question.setCourse(externalCourse)
+        def questionDetails = new MultipleChoiceQuestion()
+        question.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+        questionRepository.save(question)
+
+        optionOK = new Option()
+        optionOK.setContent(OPTION_1_CONTENT)
+        optionOK.setCorrect(true)
+        optionOK.setSequence(0)
+        optionOK.setQuestionDetails(questionDetails)
+        optionRepository.save(optionOK)
+
+        optionKO = new Option()
+        optionKO.setContent(OPTION_1_CONTENT)
+        optionKO.setCorrect(false)
+        optionKO.setSequence(1)
+        optionKO.setQuestionDetails(questionDetails)
+        optionRepository.save(optionKO)
+
+        dashboard = new Dashboard(externalCourseExecution, student)
+        dashboardRepository.save(dashboard)
     }
 
     def "student removes a difficult question from the dashboard"() {
-        
+        given:
+        def difficultQuestion = new DifficultQuestion()
+        difficultQuestion.setDashboard(dashboard)
+        difficultQuestion.setQuestion(question)
+        difficultQuestion.setPercentage(24)
+        difficultQuestion.setRemovedDate(DateHandler.now().minusDays(daysSince))
+        difficultQuestion.setRemoved(true)
+        difficultQuestionRepository.save(difficultQuestion)
+
+        when:
+        difficultQuestionService.removeDifficultQuestion(difficultQuestion.getId())
+
+        then:
+        difficultQuestionRepository.count() == 0
+        and:
+        def dashboard = dashboardRepository.getById(dashboard.getId())
+        dashboard.getDifficultQuestions().size() == 0
+
+        where:
+        daysSince << [0, 1, 6]
     }
 
+    @Unroll
     def "the difficult question cannot be deleted days before #daysSince or not removed #removed"() {
+        given: "a difficult question"
+        def difficultQuestion = new DifficultQuestion()
+        difficultQuestion.setDashboard(dashboard)
+        difficultQuestion.setQuestion(question)
+        difficultQuestion.setPercentage(24)
+        difficultQuestion.setRemovedDate(DateHandler.now().minusDays(daysSince))
+        difficultQuestion.setRemoved(removed)
+        difficultQuestionRepository.save(difficultQuestion)
 
+        when:
+        difficultQuestionService.removeDifficultQuestion(difficultQuestion.getId())
+
+        then:
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == errorMessage
+        and:
+        difficultQuestionRepository.count() == 1
+
+        where:
+        removed | daysSince || errorMessage
+        false   | 0         || CANNOT_REMOVE_DIFFICULT_QUESTION
+        true    | 7         || CANNOT_REMOVE_DIFFICULT_QUESTION
+        true    | 8         || CANNOT_REMOVE_DIFFICULT_QUESTION
     }
 
-    def "the difficult question cannot be deleted because of invalid id"() {
-        
+    @Unroll
+    def "the difficult question cannot be deleted because invalid id"() {
+        when:
+        difficultQuestionService.removeDifficultQuestion(id)
+
+        then: "an exception is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == errorMessage
+
+        where:
+        id  || errorMessage
+        0   || DIFFICULT_QUESTION_NOT_FOUND
+        100 || DIFFICULT_QUESTION_NOT_FOUND
     }
 
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
 }
