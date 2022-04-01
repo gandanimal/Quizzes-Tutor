66a4232dcd3404f38a54f52202c36cfe8804cd1c
gandanimal <gandanimal@DESKTOP-2UF84OE.localdomain>
2022-04-01 16:44:56 +0100

    F2.6: Update REST Service Test - Weekly Score #81 - student cant update another students weekly score & demo teacher does not have access + setup

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/webservice/UpdateWeeklyScoreWebServiceIT.groovy
@@ -0,0 +1,69 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.webservice
+
+import groovyx.net.http.HttpResponseException
+import groovyx.net.http.RESTClient
+import org.apache.http.HttpStatus
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UpdateWeeklyScoreWebServiceIT extends SpockTest {
+    @LocalServerPort
+    private int port
+
+    def response
+
+    def authUserDto
+    def courseExecutionDto
+    def dashboardDto
+
+    def setup() {
+        given:
+        restClient = new RESTClient("http://localhost:" + port)
+        and:
+        courseExecutionDto = courseService.getDemoCourse()
+        authUserDto = authUserService.demoStudentAuth(false).getUser()
+        dashboardDto = dashboardService.getDashboard(courseExecutionDto.getCourseExecutionId(), authUserDto.getId())
+    }
+
+    def "demo student updates its weekly scores"() {
+    }
+
+    def "demo teacher does not have access"() {
+        given: 'demo teacher'
+        demoTeacherLogin()
+
+        when: 'the web service is invoked'
+        response = restClient.put(
+                path: '/students/dashboards/weeklyScores/' + dashboardDto.getId(),
+                requestContentType: 'application/json'
+        )
+
+        then: "request returns 403 because teacher does not have access to a student dashboard"
+        def error = thrown(HttpResponseException)
+        error.response.status == HttpStatus.SC_FORBIDDEN
+
+        cleanup:
+        weeklyScoreRepository.deleteAll()
+    }
+
+    def "student cant update another students weekly score"() {
+        given: 'a different new demo student'
+        demoNewStudentLogin()
+
+        when: 'the web service is invoked'
+        response = restClient.get(
+                path: '/students/dashboards/weeklyScores/' + dashboardDto.getId(),
+                requestContentType: 'application/json'
+        )
+
+        then: "request returns 403 because uses does not have access to another student dashboard"
+        def error = thrown(HttpResponseException)
+        error.response.status == HttpStatus.SC_FORBIDDEN
+
+        cleanup:
+        weeklyScoreRepository.deleteAll()
+    }
+
+}
